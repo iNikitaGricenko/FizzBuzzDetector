e340a8f4db9a8fb509c2667464e80fee2fd92abc
Nikita Gricenko <82543287+iNikitaGricenko@users.noreply.github.com>
2021-11-10 18:42:00 +0200

    Initial commit

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,6 +1,7 @@
 
 public class Launcher {
-    private static String input = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb It's fleece was white as snow";
+    private static String input = "Mary had a little lamb Little lamb, little lamb Mary had a little " +
+            "lamb It's fleece was white as snow";
 
     public static void main(String[] args) {
         FizzBuzzDetector fizzBuzzDetector = new FizzBuzzDetector(input);
